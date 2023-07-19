72f982a4ca44840d016321926f324d70ee191c11
Sathyaraj <r.sathyaraj@adglobal360.com>
2023-07-19 16:50:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Login from "./Login";
+import Register from "./Register";
+
+function App() {
+  return (
+    <main className="App">
+      <Login />
+    </main>
+  );
+}
+
+export default App;
